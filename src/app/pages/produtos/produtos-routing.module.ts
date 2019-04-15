84b9e4ddbdbec5279912071f0b1780d422ac27ce
Marcos Buganeme <marcos.after@gmail.com>
2019-04-15 15:36:49 -0300

    Estrutura básica
    Adicionando dois módulos
    
    Home -> página inicial
    Produto -> contem a estrutura básica para o CRUD da aplicação
    
    -> Adicionado Lazy Load para o carregamento de rotas
    
    -> Adicionando as seguintes libs para a melhoria do projeto
    * bootstrap 4
    * jquery
    * moment
    * currency_formatter
    * angular imask
    * toastr

---
+++ src/app/pages/produtos/produtos-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ProdutoListComponent } from './produto-list/produto-list.component';
+import { ProdutoFormComponent } from './produto-form/produto-form.component';
+
+const routes: Routes = [
+  { path: '', component: ProdutoListComponent },
+  { path: 'new', component: ProdutoFormComponent },
+  { path: ':id/edit', component: ProdutoFormComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProdutosRoutingModule { }
